4eedb68ec07511ad11baffc98ac765ba5c9855dc
johan003z <johan.aguilar003@gmail.com>
2026-08-12 20:02:07 -0500

    Aplicar migraciones al iniciar la aplicación

--- Program.cs
+++ Program.cs
@@ -37,4 +37,10 @@ app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run(); 
